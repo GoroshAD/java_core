e454049c4c928e0f0992c3ea5caa2341acfbe74e
Artiomka Goroschko <nikto8748@gmail.com>
2025-08-05 23:14:51 +0300

    Fixed first task

--- HW2/First/Download/src/Main.java
+++ HW2/First/Download/src/Main.java
@@ -1,77 +1,79 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         StringBuilder log = new StringBuilder();
+        String gamesPath = "C://Games";
+        List<String> gamesDirsNames = Arrays.asList("src", "res", "savegames", "temp");
+        List<File> gamesDirs = listDirsCreation(gamesPath, gamesDirsNames, log);
 
-        File src = new File("C://Games", "src");
-        File res = new File("C://Games", "res");
-        File savegames = new File("C://Games", "savegames");
-        File temp = new File("C://Games", "temp");
-        if (src.mkdir()) {
-            log.append("C://Games/src created\n");
+        String srcPath = gamesDirs.get(0).toString();   // get path of src
+        List<String> srcDirsNames = Arrays.asList("main", "test");
+        List<File> srcDirs = listDirsCreation(srcPath, srcDirsNames, log);
+
+        String mainPath = srcDirs.get(0).toString();
+        List<String> mainFilesNames = Arrays.asList("Main.java", "Utils.java");
+        List<File> mainFiles = listFilesCreation(mainPath, mainFilesNames, log);
+
+        String resPath = gamesDirs.get(1).toString();
+        List<String> resDirsNames = Arrays.asList("drawables", "vectors", "icons");
+        List<File> resDirs = listDirsCreation(resPath, resDirsNames, log);
+
+        String tempPath = gamesDirs.get(3).toString();
+        List<String> tempFilesNames = Arrays.asList("temp.txt");
+        List<File> tempFiles = listFilesCreation(tempPath, tempFilesNames, log);
+
+        logging(tempPath, "temp.txt", log);
+    }
+
+    public static List<File> listDirsCreation(String path, List<String> names, StringBuilder log) {
+        List<File> res = new ArrayList<>();
+        for (String dirName : names) {
+            res.add(dirCreation(path, dirName, log));
         }
+        return res;
+    }
+
+    public static List<File> listFilesCreation(String path, List<String> names, StringBuilder log) {
+        List<File> res = new ArrayList<>();
+        for (String fileName : names) {
+            res.add(fileCreation(path, fileName, log));
+        }
+        return res;
+    }
+
+    public static File dirCreation(String path, String dirName, StringBuilder log) {
+        File res = new File(path, dirName);
         if (res.mkdir()) {
-            log.append("C://Games/res created\n");
+            log.append((String) (path + '/' + dirName + " created.\n"));
         }
-        if (savegames.mkdir()) {
-            log.append("C://Games/savegames created\n");
-        }
-        if (temp.mkdir()) {
-            log.append("C://Games/temp created\n");
-        }
+        return res;
+    }
 
-        File main = new File(src, "main");
-        File test = new File(src, "test");
-        if (main.mkdir()) {
-            log.append("C://Games/src/main created\n");
-        }
-        if (test.mkdir()) {
-            log.append("C://Games/src/test created\n");
-        }
-
-        File mainJava = new File(main, "Main.java");
-        File utilsJava = new File(main, "Utils.java");
+    public static File fileCreation(String path, String fileName, StringBuilder log) {
+        File res = new File(path, fileName);
         try {
-            if (mainJava.createNewFile()) {
-                log.append("C://Games/src/main/Main.java created\n");
-            }
-            if (utilsJava.createNewFile()) {
-                log.append("C://Games/src/main/Utils.java created\n");
+            if (res.createNewFile()) {
+                log.append((String) (path + '/' + fileName + " created.\n"));
             }
         } catch (IOException e) {
-            log.append((String) ("Error: " + e.getMessage()));
+            log.append((String) ("Error: " + e.getMessage() + '\n'));
         }
+        return res;
+    }
 
-        File drawables = new File(res, "drawables");
-        File vectors = new File(res, "vectors");
-        File icons = new File(res, "icons");
-        if (drawables.mkdir()) {
-            log.append("C://Games/res/drawables created\n");
-        }
-        if (vectors.mkdir()) {
-            log.append("C://Games/res/vectors created\n");
-        }
-        if (icons.mkdir()) {
-            log.append("C://Games/res/icons created\n");
-        }
-
-        File tempTxt = new File(temp, "temp.txt");
-        try {
-            if (tempTxt.createNewFile()) {
-                log.append("C://Games/temp/temp.txt created\n");
-            }
-        } catch (IOException e) {
-            log.append((String) ("Error: " + e.getMessage()));
-        }
-
+    public static void logging(String path, String fileName, StringBuilder log) {
+        String tempTxt = new StringBuilder(path).append('/').append(fileName).toString();
         try (FileWriter fw = new FileWriter(tempTxt, false)) {
             fw.write(log.toString());
             fw.flush();
         } catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("Error: " + e.getMessage() + '\n');
         }
     }
 }
